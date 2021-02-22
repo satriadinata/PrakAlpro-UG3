52e63b718d35fbc00d67769bb5c2c05adf1d57fd
satriadinata <satriadinatazr@gmail.com>
2021-02-22 11:24:16 +0700

    Update index.py

--- index.py
+++ index.py
@@ -1,11 +1,13 @@
 # satriadinata ratnanto 71200626
 # ada seorang siswa yang kesulitan untuk mencari urutan bilangan prima
 # dari n sampai n dimana dia bisa custom input bilangan itu
-awal = int(input('Masukkan bilangan awal : '))
-akhir = int(input('Masukkan bilangan akhir : '))
+awal = int(input('Masukkan angka awal : '))
+akhir = int(input('Masukkan angka akhir : '))
 result=[]
-for i in range (awal,akhir+1):
-    if i >1 and i % 2 != 0 and i % 3 != 0 and i % 5!=0 and i%6 != 0 and i%7 != 0 and i%8 !=0 and i%9!=0 or i==9 or i==8  or i==7 or i==6 or i==5 or i == 2 or i == 3 :
+for i in range(awal,akhir+1):
+    if i > 1 and i <= 10 and i % 2 !=0 and i % 3 !=0 or i == 2 or i == 3:
         result.append(i)
-print('Diatara', awal, 'dan', akhir,'ada bilangan prima yaitu', result)
+    elif i > 10 and i%2 !=0 and i%3!=0 and i%4!=0 and i%5!=0 and i%6!=0 and i%7!=0 and i%8!=0 and i%9!=0:
+        result.append(i)
+print('Diantara', awal,'sampai', akhir, 'ada bilangan prima yaitu :', result)
 print('Ada sejumlah', len(result),'bilangan')
